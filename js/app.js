ca77a138e468142967c4b77b9e0dc8b8c05afce7
Aleksey Belchenkov <u608110@gmail.com>
2019-01-12 10:19:00 +0300

    Fetch Weather From API

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Init weather object
+const weather = new Weather('Boston', 'MA');
+
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+function getWeather() {
+    weather.getWeather()
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+}
+
